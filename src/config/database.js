a422d0e1783dc357c6245937e118a43f83cff97b
ALUNO SENAI PETROLINA <aluno.petrolina@senai.pe>
2026-08-26 11:00:40 -0300

    Corrige conexão com MySQL

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,27 @@
-const { Sequelize } = require("sequelize");
+const mysql = require("mysql2");
+
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    logging: false,
-    dialectOptions: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
     ssl: {
         rejectUnauthorized: false
     }
-},
-  }
-);
+});
 
-module.exports = sequelize;
+connection.getConnection((erro, conexao) => {
+    if (erro) {
+        console.error("Erro ao conectar com o banco:", erro);
+        return;
+    }
+
+    console.log("Banco de dados conectado com sucesso!");
+
+    conexao.release();
+});
+
+module.exports = connection;
